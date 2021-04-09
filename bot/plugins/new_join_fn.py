b08223acb0e7e4292efd0ded9b42fb56e3de889d
source-pack <76510826+source-pack@users.noreply.github.com>
2021-04-09 14:59:43 +0430

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -74,12 +74,7 @@ async def help_message_f(client, message):
         Localisation.HELP_MESSAGE,
         reply_markup=InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
-                ],
-                [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
-                ],
+                
                 [
                     InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
                     InlineKeyboardButton('Source Code', url='https://github.com/AbirHasan2005/VideoCompress') # Must Give us Credits!
